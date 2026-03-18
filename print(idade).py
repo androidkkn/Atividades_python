b4cc55f75672ea87e2b4881dcba9ada1593fb5b9
androidkkn <kauan.kkn.2020@gmail.com>
2026-03-18 17:03:07 -0300

    atividade 18/03

---
+++ print(idade).py
@@ -0,0 +1,4 @@
+print("Qual sua idade")
+idade=int(input())
+print(f"Voce tem {idade} anos")
+
---
+++ print(nome).py
@@ -0,0 +1,6 @@
+print("Qual seu nome?")
+nome =input()
+print("Ola", nome, "!")
+print("Ola {}!".format(nome))
+print(f"Ola {nome}!")
+
