f8d9383292511ca3453ec31b516dbc3ddadd7b15
androidkkn <kauan.kkn.2020@gmail.com>
2026-04-01 16:25:28 -0300

    ...

---
+++ operacoes_mat.py
@@ -0,0 +1,9 @@
+soma = 10 + 10
+subtracao = 20 - 10
+multiplicacao = 10 * 5
+divisao = 100 / 10
+
+print(f"O valor da soma é {soma}")
+print (f"O valor da subtração é {subtracao}")
+print (f"O valor da multiplicação é {multiplicacao}")
+print (f"O valor da divisão é {divisao}")
