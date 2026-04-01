e4e2890658c442feb51964c2b4d75043352c7801
androidkkn <kauan.kkn.2020@gmail.com>
2026-04-01 17:11:31 -0300

    01/04/26

--- operacoes_mat.py
+++ operacoes_mat.py
@@ -2,8 +2,21 @@ soma = 10 + 10
 subtracao = 20 - 10
 multiplicacao = 10 * 5
 divisao = 100 / 10
+divisao_inteira = 5 // 3
+resto_divisao = 5 % 3
+potencia = 5 ** 2
+raiz_quad = 81 ** 0.5 #ou "81 ** (1/2)"
 
 print(f"O valor da soma é {soma}")
 print(f"O valor da subtração é {subtracao}")
 print(f"O valor da multiplicação é {multiplicacao}")
 print(f"O valor da divisão é {divisao}")
+print(f"O valor da divisão inteira é {divisao_inteira}")
+print(f"O resto da divisão é {resto_divisao}")
+print(f"O resultado da potencia é {potencia}")
+print(f"O valor da raiz quadrada é {raiz_quad}")
+
+# Aqui eu posso escrever o que eu quiser que não vai dar erro
+# () é um parenteses
+# [] é um colchetes
+# {} é chaves
