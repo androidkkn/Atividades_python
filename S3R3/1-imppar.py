2aef2a56d80e54ed0be0b338e4b080bddf1cf0a7
androidkkn <kauan.kkn.2020@gmail.com>
2026-04-29 17:10:51 -0300

    cmc exerc

---
+++ S3R3/1-imppar.py
@@ -0,0 +1,5 @@
+for i in range(101):
+    if i % 2 == 0:
+        print(f"{i} par")
+    else:
+        print(f"{i} impar")
--- estrutura-de-dados/dicionario3.py
+++ estrutura-de-dados/dicionario3.py
@@ -1,26 +1,26 @@
-# tupla = (1,2,3,4,5)
-# print(tupla)
+tupla = (1,2,3,4,5)
+print(tupla)
 
-#desempacotamento de tupla
-# a,b,c,d,e = tupla 
-# print(a)
-# print(b)
-# print(c)
-# print(d)
-# print(e)
+# desempacotamento de tupla
+a,b,c,d,e = tupla 
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
 
-# x,y,z = (10,20,30)
-# print(x)
-# print(y)
-# print(z)
+x,y,z = (10,20,30)
+print(x)
+print(y)
+print(z)
 
-# a= (1,2,3)
-# print(a)
-# print("Id da primeira tupla", id(a)) #o id é o mesmo, ou seja, a tupla é imutável, ou seja, não pode ser alterada, ou seja, quando tentamos alterar a tupla, na verdade estamos criando uma nova tupla e atribuindo a variável a para essa nova tupla, ou seja, a variável a passa a apontar para a nova tupla e o id da nova tupla é diferente do id da tupla original
+a= (1,2,3)
+print(a)
+print("Id da primeira tupla", id(a)) #o id é o mesmo, ou seja, a tupla é imutável, ou seja, não pode ser alterada, ou seja, quando tentamos alterar a tupla, na verdade estamos criando uma nova tupla e atribuindo a variável a para essa nova tupla, ou seja, a variável a passa a apontar para a nova tupla e o id da nova tupla é diferente do id da tupla original
 
-# a= (0,1,2)
-# print(a)
-# print("Id da segunda tupla", id(a)) #o id é diferente, ou seja, a tupla foi alterada, ou seja, a variável a passou a apontar para a nova tupla e o id da nova tupla é diferente do id da tupla original
+a= (0,1,2)
+print(a)
+print("Id da segunda tupla", id(a)) #o id é diferente, ou seja, a tupla foi alterada, ou seja, a variável a passou a apontar para a nova tupla e o id da nova tupla é diferente do id da tupla original
 
 c= (1,2,3)
 d= c
